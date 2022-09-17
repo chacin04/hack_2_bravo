e468b7243944b666c05593936e059a8ae0a3806f
Ricardo Bermudez <rbermudez.coder@gmail.com>
2022-09-17 10:08:38 -0400

    Upload: borrando archivos duplicados

--- bravohack1.js
+++
@@ -1,6 +0,0 @@
-let bravoArray = ["100","200","300","400","500","600","700"]
-
-console.log(bravoArray)
-//Muestra Array original 
-console.log(bravoArray.splice())
-//Muestra Array Vacio  
--- bravohack8.js
+++
@@ -1,7 +0,0 @@
-let bravoArray = ["100","200","300","400","500","600","700"]
-//                  0     1     2     3     4     5     6 
-console.log(bravoArray)
-
-bravoArray.push ("bravo")
-
-console.log(bravoArray)
