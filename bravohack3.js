c4706f109a96a480b5b59560df7b5228abf8c0ec
Ricardo Bermudez <rbermudez.coder@gmail.com>
2022-09-16 23:14:42 -0400

    Upload: cargando bravohack3 file

---
+++ bravohack3.js
@@ -0,0 +1,7 @@
+let bravoArray = ["100","200","300","400","500","600","700"]
+
+console.log(bravoArray)
+//Muestra Array original 
+bravoArray.pop()
+//Borra el Primer item del Array 
+console.log(bravoArray)
