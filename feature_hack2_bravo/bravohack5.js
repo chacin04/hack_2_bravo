77da9895b1fa80bc386011ff72a2bf2391f087cb
Ricardo Bermudez <rbermudez.coder@gmail.com>
2022-09-17 09:54:01 -0400

    Upload: Cargando directorio con los hacks

---
+++ feature_hack2_bravo/bravohack5.js
@@ -0,0 +1,8 @@
+let bravoArray = ["100","200","300","400","500","600","700"]
+//                  0     1     2     3     4     5     6 
+console.log(bravoArray)
+
+let clonBravoArray = [...bravoArray]
+//Creo nuevo contenedor y le inserto el contenido de bravoArray
+console.log(clonBravoArray)
+//Muestro el resultado apuntando al nuevo contenedor
