e85cbe66f1f53d743f02206827b6c11b1521ba12
Ricardo Bermudez <rbermudez.coder@gmail.com>
2022-09-16 23:55:05 -0400

    Upload: Cargando bravohack9 y bravohack10 files

---
+++ bravohack9.js
@@ -0,0 +1,9 @@
+let bravoArray = ["100","200","300","400","500","600","700"]
+//                  0     1     2     3     4     5     6 
+console.log(bravoArray)
+
+bravoArray.unshift("bravo")
+//Se agrega alias al inicio del array 
+
+console.log(bravoArray)
+//Se muestra el resultado del nuevo Array 
