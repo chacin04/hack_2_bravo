dbb781ea1b5061d07b085de36808aeebccde83fb
Ricardo Bermudez <rbermudez.coder@gmail.com>
2022-09-16 23:46:44 -0400

    Upload: cargando bravohack7 file

---
+++ bravohack7.js
@@ -0,0 +1,9 @@
+let bravoArray = ["100","200","300","400","500","600","700"]
+//                  0     1     2     3     4     5     6 
+console.log(bravoArray)
+
+bravoArray.splice(2,1,"bravo")
+//Se elimina el Item 300 y se reemplaza por 300 
+
+console.log(bravoArray)
+//Se muestra el resultado del nuevo Array 
